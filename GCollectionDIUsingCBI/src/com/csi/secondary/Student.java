08cc06d919b22b4ec4bcafb4546fcd8e321db25b
Shwetavishwase <147078113+Shwetavishwase@users.noreply.github.com>
2024-08-08 12:06:20 +0530

    Add files via upload

---
+++ GCollectionDIUsingCBI/src/com/csi/secondary/Student.java
@@ -0,0 +1,56 @@
+package com.csi.secondary;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Student {
+
+	List<Integer> list;
+
+	Set<String> set;
+
+	Map<Integer, String> map;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(List<Integer> list, Set<String> set, Map<Integer, String> map) {
+		super();
+		this.list = list;
+		this.set = set;
+		this.map = map;
+	}
+
+	public List<Integer> getList() {
+		return list;
+	}
+
+	public void setList(List<Integer> list) {
+		this.list = list;
+	}
+
+	public Set<String> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<String> set) {
+		this.set = set;
+	}
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
+	}
+
+	@Override
+	public String toString() {
+		return " Student [list=" + list + ",  set=" + set + ", map=" + map + "]";
+	}
+
+}
